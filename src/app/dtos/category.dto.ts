7c959bf0816b51a2ddbe77d0dec68ee58862d17b
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-08-02 11:30:38 -0500

    Decoradores.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/app/dtos/category.dto.ts
+++ src/app/dtos/category.dto.ts
@@ -1,10 +1,38 @@
-import { Category } from '../models/category.model';
+import { IsEnum, IsNotEmpty, IsOptional, IsUrl, Length, validateOrReject } from 'class-validator';
+
+import { AccessType, Category } from '../models/category.model';
 
 // Objetos de transferencia de datos.
-export interface CreateCategoryDTO extends Omit<Category, 'id'> {
+export interface ICreateCategoryDTO extends Omit<Category, 'id'> {
 }
 
-export interface UpdateCategoryDTO extends Partial<CreateCategoryDTO> { }
+export class CreateCategoryDTO implements ICreateCategoryDTO {
+
+    @IsNotEmpty()
+    @Length(2, 255)
+    name!: string;
+
+    @IsUrl()
+    @IsNotEmpty()
+    image!: string;
+
+    @IsOptional()
+    @IsEnum(AccessType)
+    access?: AccessType;
+}
+
+export interface UpdateCategoryDTO extends Partial<ICreateCategoryDTO> { }
 
 export interface FindCategoryDTO extends Readonly<Category> {
 }
+
+(async () => {
+    try {
+        const dto = new CreateCategoryDTO();
+        dto.name = 'Test';
+        dto.image = 'https://cdn.lorem.space/images/fashion/.cache/640x480/roland-hechanova-1eedDSknxoY-unsplash.jpg';
+        await validateOrReject(dto);
+    } catch (error) {
+        console.error(error);
+    }
+})();
--- src/app/models/category.model.ts
+++ src/app/models/category.model.ts
@@ -1,5 +1,11 @@
+export enum AccessType {
+    PRIVATE = 'private',
+    PUBLIC = 'public'
+}
+
 export interface Category {
     id: number | string;
     name: string;
     image: string;
+    access?: AccessType;
 }
